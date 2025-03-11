5e84929fcf5a27481e1cc5805c1558b89e6b65cd
AloneDineshEver <108724696+Dinesh89-D@users.noreply.github.com>
2025-03-11 11:50:00 +0530

    demo5

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
+import userRoutes from './Routes/user.Route.js'
 dotenv.config();
 mongoose.connect(process.env.MONG0_DB).then(()=>{
     console.log('Connected to MONGODB');
@@ -12,3 +13,5 @@ const app = express();
 app.listen(3000,()=>{
     console.log('Sever listening on port 3000!');
 });
+
+app.use("/api/user",userRoutes)
