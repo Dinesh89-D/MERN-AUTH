0f014cd5eac15692bbe0a6fdb1971aa19b93f2d3
AloneDineshEver <108724696+Dinesh89-D@users.noreply.github.com>
2025-03-11 16:53:23 +0530

    demo11

--- api/Routes/auth.route.js
+++ api/Routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { signup } from '../Controllers/auth.Controller.js';
+import { signin, signup } from '../Controllers/auth.Controller.js';
 const router = express.Router();
 router.post("/signup",signup)
+router.post("/signin",signin)
 export default router;
