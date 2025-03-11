f304e45950542f873095cadfa1e0484293beb1cf
AloneDineshEver <108724696+Dinesh89-D@users.noreply.github.com>
2025-03-11 11:12:35 +0530

    demo3

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONG0_DB).then(()=>{
+    console.log('Connected to MONGODB');
+}).catch((err)=>{
+    console.log(err);
+})
+
 const app = express();
 app.listen(3000,()=>{
-    console.log('Sever listening on port 3000');
+    console.log('Sever listening on port 3000!');
 })
