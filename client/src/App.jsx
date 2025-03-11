5b5fff8b560bd6a9587f298cbbe45c60156cccdc
AloneDineshEver <108724696+Dinesh89-D@users.noreply.github.com>
2025-03-11 10:20:41 +0530

    demo1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
 
 const App = () => {
   return (
-    <h1 className='text-red-950'>App</h1>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+          <Route path='/sign-in' element={<Signin/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
